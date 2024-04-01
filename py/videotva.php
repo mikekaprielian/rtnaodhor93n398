9dfed5c4f0a90c6c1be305525b4104319ee414ee
mikekaprielian <mikekaprielian@vif.com>
2024-04-01 10:56:35 -0400

    Update videotva.php

--- py/videotva.php
+++ py/videotva.php
@@ -1,7 +1,7 @@
 <?php
 
 // Run the Python script and capture its output
-$output = shell_exec('python3 videotva.py');
+$output = shell_exec('python3 https://raw.githubusercontent.com/mikekaprielian/rtnaodhor93n398/main/py/videotva.py');
 
 // Forward the PHP page to the obtained link
 header("Location: $output");
