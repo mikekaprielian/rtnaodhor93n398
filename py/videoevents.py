9fb4d694092797908bd77eddba38da5bd7dac4d1
mikekaprielian <mikekaprielian@vif.com>
2024-04-12 13:32:27 -0400

    Update videoevents.py

--- py/videoevents.py
+++ py/videoevents.py
@@ -9,6 +9,18 @@ from webdriver_manager.core.os_manager import ChromeType
 import random
 import time
 import json
+import pytz
+from datetime import datetime
+
+# Function to convert UTC time to Eastern Time Zone (EST)
+def utc_to_est(utc_time_str):
+    # Parse the UTC time string
+    utc_time = datetime.strptime(utc_time_str, "%m/%d/%y %I:%M:%S %p UTC")
+    # Convert UTC time to Eastern Time Zone (EST)
+    est_time = utc_time.replace(tzinfo=pytz.utc).astimezone(pytz.timezone('US/Eastern'))
+    # Format the EST time string
+    est_time_str = est_time.strftime("%m/%d/%y %I:%M:%S %p EST")
+    return est_time_str
 
 user_agents = [
     #add your list of user agents here
@@ -106,10 +118,12 @@ for group, name, link in all_links:
 
     # Print the collected m3u8 URLs
     if m3u8_urls:
-        name_fixed = name.replace(',', '')
-        name_fixed = name.replace(': ', ' -')
-        print(f"#EXTINF:-1 group-title=\"{group}\", {name_fixed}")
-        print(m3u8_urls[0])  # Print only the first m3u8 URL
+     name_fixed = name.replace(',', '')
+     name_fixed = name_fixed.replace(': ', ' -')
+     est_time_str = utc_to_est(name_fixed)
+     print(f"#EXTINF:-1 group-title=\"{group}\", {est_time_str}")
+     print(m3u8_urls[0])  # Print only the first m3u8 URL
+
 
 # Close the WebDriver
 driver.quit()
