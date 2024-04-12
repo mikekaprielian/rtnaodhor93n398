ad56d2faaecc6388b81b5f5cb3e33647863904cd
mikekaprielian <mikekaprielian@vif.com>
2024-04-12 09:44:52 -0400

    Update videoevents.py

--- py/videoevents.py
+++ py/videoevents.py
@@ -5,7 +5,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium_stealth import stealth
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.utils import ChromeType
+from webdriver_manager.core.os_manager import ChromeType
 import random
 import time
 
