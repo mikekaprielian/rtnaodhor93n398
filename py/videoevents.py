7763fa742df53a2de12ffa0a53e88a8102a71ae6
mikekaprielian <mikekaprielian@vif.com>
2024-04-12 10:23:02 -0400

    Update videoevents.py

--- py/videoevents.py
+++ py/videoevents.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 from selenium_stealth import stealth
 from webdriver_manager.chrome import ChromeDriverManager
