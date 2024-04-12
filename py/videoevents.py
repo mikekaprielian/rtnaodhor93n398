2f93190e30028525fd0fd941b6b736b7356164c0
mikekaprielian <mikekaprielian@vif.com>
2024-04-12 10:19:36 -0400

    Update videoevents.py

--- py/videoevents.py
+++ py/videoevents.py
@@ -18,15 +18,28 @@ user_agents = [
 chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
 
 # Set Chrome options
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-gpu")
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("start-maximized")
-chrome_options.add_argument("disable-infobars")
-chrome_options.add_argument("--disable-extensions")
-chrome_options.add_argument("--crash-dumps-dir=/tmp")
+# chrome_options = webdriver.ChromeOptions()
+# chrome_options.add_argument("--headless")
+# chrome_options.add_argument("--disable-gpu")
+# chrome_options.add_argument("--no-sandbox")
+# chrome_options.add_argument("--disable-dev-shm-usage")
+# chrome_options.add_argument("start-maximized")
+# chrome_options.add_argument("disable-infobars")
+# chrome_options.add_argument("--disable-extensions")
+# chrome_options.add_argument("--crash-dumps-dir=/tmp")
+
+chrome_options = Options()
+options = [
+    "--headless",
+    "--disable-gpu",
+    "--window-size=1920,1200",
+    "--ignore-certificate-errors",
+    "--disable-extensions",
+    "--no-sandbox",
+    "--disable-dev-shm-usage"
+]
+for option in options:
+    chrome_options.add_argument(option)
 
 # Randomly select a user agent
 user_agent = random.choice(user_agents)
